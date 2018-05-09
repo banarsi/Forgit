6cabfd82481c3ec7333fa9e7528924d5eb4468f0
banarsi <36882931+banarsi@users.noreply.github.com>
2018-05-09 15:05:30 +0530

    abcd

--- Forgit/src/Sample.java
+++ Forgit/src/Sample.java
@@ -5,10 +5,7 @@ public class Sample
 public static void main(String[]args)
 {
 
-	System.out.println("hello eclipse");
-
-	System.out.println("hi eclipse");
-	System.out.println("bye eclipse");
+	
 	
 	
 
