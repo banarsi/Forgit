a02c687223c4722a40c02dc46f46690fe7b33136
tyss <tyss@localhost>
2018-05-09 14:58:19 +0530

    commit last

--- Forgit/src/Sample.java
+++ Forgit/src/Sample.java
@@ -5,20 +5,15 @@ public class Sample
 public static void main(String[]args)
 {
 	System.out.println("hello eclipse");
-<<<<<<< HEAD
-	
-	System.out.println("hi eclipse");
-=======
-	System.out.println("hello git");
->>>>>>> branch 'master' of https://github.com/banarsi/Forgit.git
-	
-<<<<<<< HEAD
-	
-	System.out.println("hello git");
-	
-=======
 
->>>>>>> branch 'master' of https://github.com/banarsi/Forgit.git
+	System.out.println("hi eclipse");
+	System.out.println("bye eclipse");
+	
+	
+
+	System.out.println("hello git");
+System.out.println("hi git");
+	System.out.println("bye git");
 	
 
 
