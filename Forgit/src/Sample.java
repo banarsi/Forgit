9798f68f146ed7be45f8be4103b60531c2803d56
tyss <tyss@localhost>
2018-05-09 14:12:36 +0530

    conflict

--- Forgit/src/Sample.java
+++ Forgit/src/Sample.java
@@ -6,12 +6,9 @@ public static void main(String[]args)
 {
 	System.out.println("hello eclipse");
 	
-<<<<<<< HEAD
-	System.out.println("hi eclipse");
-	
-=======
->>>>>>> refs/remotes/origin/master
 	System.out.println("hello git");
+	System.out.println("hi git");
+	
 	
 	
 }
