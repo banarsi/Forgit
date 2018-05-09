9d08fe452d393ff45004509926007d68ef2e42e7
banarsi <36882931+banarsi@users.noreply.github.com>
2018-05-09 14:51:11 +0530

    git changes

--- Forgit/src/Sample.java
+++ Forgit/src/Sample.java
@@ -4,21 +4,9 @@ public class Sample
 {
 public static void main(String[]args)
 {
-	System.out.println("hello eclipse");
-<<<<<<< HEAD
-	
-	System.out.println("hi eclipse");
-=======
 	System.out.println("hello git");
->>>>>>> branch 'master' of https://github.com/banarsi/Forgit.git
-	
-<<<<<<< HEAD
-	
-	System.out.println("hello git");
-	
-=======
-
->>>>>>> branch 'master' of https://github.com/banarsi/Forgit.git
+System.out.println("hi git");
+	System.out.println("bye git");
 	
 
 
